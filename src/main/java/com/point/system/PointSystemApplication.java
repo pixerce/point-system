9d9691b1e9a692c7d2eff1a2a57c325f14c1413b
jongseok <pixerce@gmail.com>
2025-11-20 14:04:29 +0900

    initialize project

---
+++ src/main/java/com/point/system/PointSystemApplication.java
@@ -0,0 +1,13 @@
+package com.point.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/point/system/PointSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.point.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
